c1b6fdf8fa8df7f327ca626684f5e03e1b71725e
Raphael Queiroz <rapha95@gmail.com>
2019-05-13 18:03:25 -0300

    Alterado argumentos do RandomForestClassifier

--- trab2.py
+++ trab2.py
@@ -17,6 +17,10 @@ y = df.iloc[:,0]
 sc = StandardScaler()  
 x = sc.fit_transform(x)
 clf=RandomForestClassifier(n_estimators=100)
+# classifica o objeto considerando as impuresas existentes entre os nós
+clf2=RandomForestClassifier(n_estimators=100, min_impurity_decrease=1e-7)
+# classifica o objeto utilizando o critério de Entropia
+clf3=RandomForestClassifier(n_estimators=100, criterion='entropy')
 
 # treina o modelo com 80% dos dados, prediz o de 20% (dados teste) e cacula acurácia 
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
@@ -27,4 +31,26 @@ print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 # faz o kfold, treina e prediz para cada fold, calcula o score e retorna um array de todos os scores.
 kf = StratifiedKFold(n_splits=10)
 scores = cross_val_score(clf, x, y, cv=kf)
+print("Score: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+# treina o modelo com 80% dos dados, prediz o de 20% (dados teste) e cacula acurácia 
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
+clf2.fit(x_train,y_train)
+y_pred=clf2.predict(x_test)
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+# faz o kfold, treina e prediz para cada fold, calcula o score e retorna um array de todos os scores.
+kf = StratifiedKFold(n_splits=10)
+scores = cross_val_score(clf2, x, y, cv=kf)
+print("Score: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+# treina o modelo com 80% dos dados, prediz o de 20% (dados teste) e cacula acurácia 
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
+clf3.fit(x_train,y_train)
+y_pred=clf3.predict(x_test)
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+# faz o kfold, treina e prediz para cada fold, calcula o score e retorna um array de todos os scores.
+kf = StratifiedKFold(n_splits=10)
+scores = cross_val_score(clf3, x, y, cv=kf)
 print("Score: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
